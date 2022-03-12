36d496e9eba4b4f8cb340b1f05111684519f1b75
Chilk Castro <chilcjcastro@yahoo.ca>
2022-03-12 12:27:55 -0500

    Edited something on the Login index.php view

--- app/views/Login/create.php
+++ app/views/Login/create.php
@@ -27,7 +27,7 @@
     </div>
     
     <button type="submit" name="signup" class="btn btn-primary mt-2"> Sign up</button>
-    <p class="text-center">Already registered? <a href="/MVC/Login/">Login</a> </p>
+    <p class="text-center">Already registered? <a href="/Blog/Login/">Login</a> </p>
 
     <?php
 
