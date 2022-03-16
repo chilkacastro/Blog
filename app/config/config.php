5a850016d11b4a29c17ec1e6dafed49b561258c1
Chilka Castro <chilcjcastro@yahoo.ca>
2022-03-16 14:24:53 -0400

    commented a line in config file

--- app/config/config.php
+++ app/config/config.php
@@ -10,4 +10,5 @@
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', '');
-  define('DB_NAME', 'blog_database');
+  // define('DB_NAME', 'blog_database');
+  define('DB_NAME', 'blog');
