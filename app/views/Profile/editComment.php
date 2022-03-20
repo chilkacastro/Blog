872c4d44732d2f6fb68f67d04773eb5d973e1365
phgthanhng <phuongthanhnguyen136@gmail.com>
2022-03-20 11:59:02 -0400

    syntax error

--- app/views/Profile/editComment.php
+++ app/views/Profile/editComment.php
@@ -8,14 +8,14 @@
                     <div class="card-body p-5">
                          <form class="container-fluid" action='' method='post' enctype="multipart/form-data">
                             <h3 class="mb-3 d-flex justify-content-center">EDIT COMMENT</h3>
-                          
+                            
                             <div class="form-outline mb-4">
                                 <label for="updateComment">Your comment</label>
                                 <input name="commentText" type="text" class="form-control" id="commentinput" value="<?php echo $data["existing_comment"]->publication_comment_text?>">
                             </div>
-
+                         
                             <div class="d-flex justify-content-center">
-                                <button type="editComment" name='editComment' class="btn btn-primary">Save</button>
+                                <button type="submit" name='editComment' class="btn btn-primary">Save</button>
                             </div>
 
                         </form>
